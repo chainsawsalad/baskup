3c281468b62a3ed3346ee9084d5844954b17339e
Jeff Colombo <jeff@colombo.us>
2016-01-15 15:10:36 -0500

    Make working directory generic, format output text better

--- backUpMessages.sh
+++ backUpMessages.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
 #  backUpMessages.sh
-#  
+#
 #
 #  Created by Peter Kaminski on 7/17/15.
 # Main code can be found at https://github.com/kyro38/MiscStuff/blob/master/OSXStuff/.bashrc
 # However I have tweaked it a bit to optimize the result
+
+cwd=`pwd`
 
 #Read all information that our python script dug up
 while IFS='' read -r line || [[ -n $line ]]; do
@@ -13,24 +15,31 @@ while IFS='' read -r line || [[ -n $line ]]; do
 contact=$line
 arrIN=(${contact//;/ })
 contactNumber=${arrIN[2]}
-#Get to the home directory
-cd
-#This path should be the same as the path you use in baskup.sh
-cd ./Downloads/baskup-master
+#Get to the working directory directory
+cd $cwd
 #Make a directory specifically for this folder
-mkdir $contactNumber
+mkdir -p $contactNumber
 #Now get into the directory
 cd $contactNumber
 #Perform SQL operations
 sqlite3 ~/Library/Messages/chat.db "
-select is_from_me,text from message where handle_id=(
-select handle_id from chat_handle_join where chat_id=(
-select ROWID from chat where guid='$line')
-)" | sed 's/1\|/Me: /g;s/0\|/Friend: /g' > $line.txt
+select message.is_from_me,
+    handle.id as handle_name,
+    '(',
+    datetime(message.date + strftime('%s', '2001-01-01 00:00:00'), 'unixepoch', 'localtime') as date,
+    '): ',
+    message.text
+  from message
+  join handle on handle.ROWID=message.handle_id
+  where message.handle_id=(
+    select chat_handle_join.handle_id from chat_handle_join where chat_handle_join.chat_id=(
+      select chat.ROWID from chat where chat.guid='$line'
+    )
+  )" | sed 's/^1\|[^|]*\|/Me/;s/^0\|//;s/\|//g' > $line.txt
 
-cd
-cd ./Downloads/baskup-master/$contactNumber
-mkdir "Attachments"
+cd $cwd
+cd $contactNumber
+mkdir -p "Attachments"
 cd "Attachments"
 #Retrieve the attached stored in the local cache
 
--- baskup.sh
+++ baskup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #  baskup.sh
-#  
+#
 #
 #  Created by Peter Kaminski on 7/17/15. http://github.com/peterkaminski09
 #
@@ -13,10 +13,9 @@ select * from chat;
 ENDOFSQL
 
 #Now we need to run the python script that will parse this file and store all phone numbers
-cd
-
-#IMPORTANT: THIS SHOULD BE THE DIRECTORY OF YOUR BASKUP DOWNLOAD. If you have moved baskup to your desktop, it may need to be ./Downloads/baskup-master. Just keep this in mind
-cd ./Downloads/baskup-master
 python parseContacts.py
 
-bash backUpMessages.sh /tmp/pyContacts.txt
+cwd=export_`date +"%FT%T"`
+mkdir -p $cwd
+cd $cwd
+bash ../backUpMessages.sh /tmp/pyContacts.txt
